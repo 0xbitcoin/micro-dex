72bd3b43fea4631251c63fb6c6c8ef34ca905e0d
andy <andy@jonezie.com>
2018-04-02 21:17:57 -0400

    trimmed all the fat including fees aand the use of Ether

--- contracts/MicroDex.sol
+++ contracts/MicroDex.sol
@@ -128,38 +128,14 @@ contract ReserveToken is StandardToken, SafeMath {
   }
 }
 
-contract AccountLevels {
-  //given a user, returns an account level
-  //0 = regular user (pays take fee and make fee)
-  //1 = market maker silver (pays take fee, no make fee, gets rebate)
-  //2 = market maker gold (pays take fee, no make fee, gets entire counterparty's take fee as rebate)
-  function accountLevel(address user) constant returns(uint) {}
-}
-
-contract AccountLevelsTest is AccountLevels {
-  mapping (address => uint) public accountLevels;
-
-  function setAccountLevel(address user, uint level) {
-    accountLevels[user] = level;
-  }
-
-  function accountLevel(address user) constant returns(uint) {
-    return accountLevels[user];
-  }
-}
-
-contract _0xDex is SafeMath {
-
-  address basePairToken;
-  //check account balances for this token; we do not use ether
 
 
-  address public admin; //the admin address
-  address public feeAccount; //the account that will receive fees
-  address public accountLevelsAddr; //the address of the AccountLevels contract
-  uint public feeMake; //percentage times (1 ether)
-  uint public feeTake; //percentage times (1 ether)
-  uint public feeRebate; //percentage times (1 ether)
+
+contract MicroDex is SafeMath {
+
+   bool locked;
+
+
   mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
   mapping (address => mapping (bytes32 => bool)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
   mapping (address => mapping (bytes32 => uint)) public orderFills; //mapping of user accounts to mapping of order hashes to uints (amount of order that has been filled)
@@ -170,68 +146,20 @@ contract _0xDex is SafeMath {
   event Deposit(address token, address user, uint amount, uint balance);
   event Withdraw(address token, address user, uint amount, uint balance);
 
-  function _0xDex(address admin_, address feeAccount_, address accountLevelsAddr_, uint feeMake_, uint feeTake_, uint feeRebate_) {
-    basePairToken = 0xb6ed7644c69416d67b522e20bc294a9a9b405b31;
+  function MicroDex( ) {
+//    basePairToken = 0xb6ed7644c69416d67b522e20bc294a9a9b405b31;
+    admin = msg.sender;
 
-
-
-    admin = admin_;
-    feeAccount = feeAccount_;
-    accountLevelsAddr = accountLevelsAddr_;
-    feeMake = feeMake_;
-    feeTake = feeTake_;
-    feeRebate = feeRebate_;
+    if(locked)revert();
+    locked = true;
   }
 
   function() {
     throw;
   }
 
-  function changeAdmin(address admin_) {
-    if (msg.sender != admin) throw;
-    admin = admin_;
-  }
 
-  function changeAccountLevelsAddr(address accountLevelsAddr_) {
-    if (msg.sender != admin) throw;
-    accountLevelsAddr = accountLevelsAddr_;
-  }
-
-  function changeFeeAccount(address feeAccount_) {
-    if (msg.sender != admin) throw;
-    feeAccount = feeAccount_;
-  }
-
-  function changeFeeMake(uint feeMake_) {
-    if (msg.sender != admin) throw;
-    if (feeMake_ > feeMake) throw;
-    feeMake = feeMake_;
-  }
-
-  function changeFeeTake(uint feeTake_) {
-    if (msg.sender != admin) throw;
-    if (feeTake_ > feeTake || feeTake_ < feeRebate) throw;
-    feeTake = feeTake_;
-  }
-
-  function changeFeeRebate(uint feeRebate_) {
-    if (msg.sender != admin) throw;
-    if (feeRebate_ < feeRebate || feeRebate_ > feeTake) throw;
-    feeRebate = feeRebate_;
-  }
-
-  function deposit() payable {
-    tokens[0][msg.sender] = safeAdd(tokens[0][msg.sender], msg.value);
-    Deposit(0, msg.sender, msg.value, tokens[0][msg.sender]);
-  }
-
-  function withdraw(uint amount) {
-    if (tokens[0][msg.sender] < amount) throw;
-    tokens[0][msg.sender] = safeSub(tokens[0][msg.sender], amount);
-    if (!msg.sender.call.value(amount)()) throw;
-    Withdraw(0, msg.sender, amount, tokens[0][msg.sender]);
-  }
-
+  //call this using ApproveAndCall if possible !
   function depositToken(address token, uint amount) {
     //remember to call Token(address).approve(this, amount) or this contract will not be able to do the transfer on your behalf.
     if (token==0) throw;
@@ -272,17 +200,17 @@ contract _0xDex is SafeMath {
   }
 
   function tradeBalances(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) private {
-    uint feeMakeXfer = safeMul(amount, feeMake) / (1 ether);
-    uint feeTakeXfer = safeMul(amount, feeTake) / (1 ether);
-    uint feeRebateXfer = 0;
-    if (accountLevelsAddr != 0x0) {
-      uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
-      if (accountLevel==1) feeRebateXfer = safeMul(amount, feeRebate) / (1 ether);
-      if (accountLevel==2) feeRebateXfer = feeTakeXfer;
-    }
-    tokens[tokenGet][msg.sender] = safeSub(tokens[tokenGet][msg.sender], safeAdd(amount, feeTakeXfer));
-    tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], safeSub(safeAdd(amount, feeRebateXfer), feeMakeXfer));
-    tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeSub(safeAdd(feeMakeXfer, feeTakeXfer), feeRebateXfer));
+  //  uint feeMakeXfer = safeMul(amount, feeMake) / (1 ether);
+  //  uint feeTakeXfer = safeMul(amount, feeTake) / (1 ether);
+  //  uint feeRebateXfer = 0;
+  //  if (accountLevelsAddr != 0x0) {
+  //    uint accountLevel = AccountLevels(accountLevelsAddr).accountLevel(user);
+  //    if (accountLevel==1) feeRebateXfer = safeMul(amount, feeRebate) / (1 ether);
+  //    if (accountLevel==2) feeRebateXfer = feeTakeXfer;
+  //  }
+    tokens[tokenGet][msg.sender] = safeSub(tokens[tokenGet][msg.sender], amount);
+    tokens[tokenGet][user] = safeAdd(tokens[tokenGet][user], amount);
+  //  tokens[tokenGet][feeAccount] = safeAdd(tokens[tokenGet][feeAccount], safeSub(safeAdd(feeMakeXfer, feeTakeXfer), feeRebateXfer));
     tokens[tokenGive][user] = safeSub(tokens[tokenGive][user], safeMul(amountGive, amount) / amountGet);
     tokens[tokenGive][msg.sender] = safeAdd(tokens[tokenGive][msg.sender], safeMul(amountGive, amount) / amountGet);
   }
